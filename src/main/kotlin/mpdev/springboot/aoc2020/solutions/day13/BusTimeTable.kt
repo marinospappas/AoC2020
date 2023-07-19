eee03685f6629ca1c1a75775f192d3da91da362e
marinos <marinosch.99981@gmail.com>
2023-07-19 10:41:09 +0200

    Day 13 part 2

--- src/main/kotlin/mpdev/springboot/aoc2020/solutions/day13/BusTimeTable.kt
+++ src/main/kotlin/mpdev/springboot/aoc2020/solutions/day13/BusTimeTable.kt
@@ -34,17 +34,17 @@ class BusTimeTable(input: String) {
      * n1, n2: the number of the current round for each bus
      * k: the difference of the two timestamps
      */
-    fun busesSequence(indx: Int): Sequence<Long> {
+    fun busesSequence(index: Int): Sequence<Long> {
         ++recursionLevel
-        if (indx == 0) {
+        if (index == 0) {
             return generateSequence(1) { it + 1 }
         }
-        val id = busSchedule[indx].second
+        val id = busSchedule[index].second
         var n_1 = 0L
-        val id_1 = busSchedule[indx-1].second
-        val k = busSchedule[indx].first - busSchedule[indx-1].first
+        val id_1 = busSchedule[index-1].second
+        val k = busSchedule[index].first - busSchedule[index-1].first
         run loop@ {
-            busesSequence(indx-1).forEach {
+            busesSequence(index-1).forEach {
                 n_1 = it
                 if (((k + id_1 * n_1) % id) == 0L)
                     return@loop
@@ -54,7 +54,7 @@ class BusTimeTable(input: String) {
         return generateSequence(n) {
             // all ids are prime so increment the round by the product of all previous ids
             // this will maintain the previous buses sequence
-            it + busSchedule.subList(0,indx).fold(1L) { acc, pair -> acc * pair.second }
+            it + busSchedule.subList(0,index).fold(1L) { acc, pair -> acc * pair.second }
         }
     }
 
