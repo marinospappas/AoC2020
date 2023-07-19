74d621d7c4d993b058e41272ce02cc2a66a88d8a
marinos <marinosch.99981@gmail.com>
2023-07-19 21:49:33 +0200

    Day 15 part 2 further performance improvement by caching the information about number 0

--- src/main/kotlin/mpdev/springboot/aoc2020/solutions/day15/MemoryGame.kt
+++ src/main/kotlin/mpdev/springboot/aoc2020/solutions/day15/MemoryGame.kt
@@ -1,5 +1,7 @@
 package mpdev.springboot.aoc2020.solutions.day15
 
+import mpdev.springboot.aoc2020.utils.AocException
+
 class MemoryGame(input: List<String>) {
 
     val startSequence = input[0].split(",").map { it.toInt() }
@@ -7,27 +9,33 @@ class MemoryGame(input: List<String>) {
     val startSequenceSize = startSequence.size
     val numbersIndex = startSequence.associateWith { NumbersIndex() }.toMutableMap()
     var lastSpoken = -1
+    lateinit var lastSpokenIndex: NumbersIndex
+    var number0Index = NumbersIndex()   // information about 0 (most frequent number) is cached here
 
     fun playRound(): Int {
-        var nextNumber: Int
+        val nextNumber: Int
         if (round < startSequenceSize) {
             nextNumber = startSequence[round]
-        }
-        else {
-            nextNumber = lastSpoken
-            if (numbersIndex[nextNumber]!!.timesSpoken == 1)
+        } else {
+            if (lastSpokenIndex.timesSpoken == 1)
                 nextNumber = 0
             else
-                nextNumber = numbersIndex[nextNumber]!!.lastRoundSpoken - numbersIndex[nextNumber]!!.prevRoundSpoken
+                nextNumber = lastSpokenIndex.lastRoundSpoken - lastSpokenIndex.prevRoundSpoken
         }
-        var numberIndex: NumbersIndex?
-        if (numbersIndex[nextNumber].also { numberIndex = it } == null)
-            numberIndex = NumbersIndex()
-        numberIndex!!.prevRoundSpoken = numberIndex!!.lastRoundSpoken
-        numberIndex!!.lastRoundSpoken = round
-        numberIndex!!.timesSpoken += 1
-        numbersIndex[nextNumber] = numberIndex!!
+        var numberIndex = NumbersIndex()
+        if (nextNumber == 0)
+            numberIndex = number0Index
+        else {
+            if (numbersIndex[nextNumber] != null)
+                numberIndex = numbersIndex[nextNumber] ?: throw AocException("unexpected error 2020-15-2")
+        }
+        numberIndex.prevRoundSpoken = numberIndex.lastRoundSpoken
+        numberIndex.lastRoundSpoken = round
+        numberIndex.timesSpoken += 1
+        if (nextNumber != 0)
+            numbersIndex[nextNumber] = numberIndex
         lastSpoken = nextNumber
+        lastSpokenIndex = numberIndex
         ++round
         return nextNumber
     }
